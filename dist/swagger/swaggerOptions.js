d088c2f9a0adc32820ec1247cba12e27e7d0a9b4
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-04-02 11:26:29 +0200

    swagger documantation

--- dist/swagger/swaggerOptions.js
+++ dist/swagger/swaggerOptions.js
@@ -21,7 +21,6 @@ const options = {
             url: `http://localhost:${process.env.PORT}`
         }
     ],
-    basePath: 'brand',
     security: [
         {
             bearToken: []
@@ -42,7 +41,244 @@ const options = {
                     }
                 }
             }
-        }
+        },
+        '/Sendmessage': {
+            post: {
+                summary: "Send message",
+                tags: ["message"],
+                requestBody: {
+                    required: true,
+                    content: {
+                        'application/json': {
+                            schema: { $ref: '#/components/schemas/Message' }
+                        }
+                    }
+                }
+            }
+        },
+        '/login': {
+            post: {
+                summary: "user login",
+                tags: ["user"],
+                requestBody: {
+                    required: true,
+                    content: {
+                        'application/json': {
+                            schema: {
+                                email: { type: "string" },
+                                password: { type: "string" }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        '/reset-password': {
+            post: {
+                summary: "user reset password",
+                tags: ["user"],
+                requestBody: {
+                    required: true,
+                    content: {
+                        'application/json': {
+                            schema: {
+                                token: { type: "string" },
+                                password: { type: "string" }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        '/forgot-password': {
+            post: {
+                summary: "user request password reset",
+                tags: ["user"],
+                requestBody: {
+                    required: true,
+                    content: {
+                        'application/json': {
+                            schema: {
+                                email: { type: "string" }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        '/addblog': {
+            post: {
+                summary: "user add blog ",
+                tags: ["blog"],
+                requestBody: {
+                    required: true,
+                    content: {
+                        'application/json': {
+                            schema: { $ref: '#/components/schemas/Blog' }
+                        }
+                    }
+                }
+            }
+        },
+        '/comment/{id}': {
+            post: {
+                summary: "adding comment",
+                tags: ["blog"],
+                parameters: [
+                    {
+                        name: "id",
+                        in: "path",
+                        required: true,
+                        schema: {
+                            type: "string"
+                        }
+                    }
+                ],
+                requestBody: {
+                    required: true,
+                    content: {
+                        'application/json': {
+                            schema: { $ref: '#/components/schemas/Comment' }
+                        }
+                    }
+                }
+            }
+        },
+        '/updateBlog/{id}': {
+            patch: {
+                summary: "admin updating blog",
+                tags: ["blog"],
+                parameters: [
+                    {
+                        name: "id",
+                        in: "path",
+                        required: true,
+                        schema: {
+                            type: "string"
+                        }
+                    }
+                ],
+                requestBody: {
+                    required: true,
+                    content: {
+                        'application/json': {
+                            schema: {
+                                title: { type: "string" },
+                                content: { type: "string" }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        '/like/{id}': {
+            patch: {
+                summary: "liking blog",
+                tags: ["blog"],
+                parameters: [
+                    {
+                        name: "id",
+                        in: "path",
+                        required: true,
+                        schema: {
+                            type: "string"
+                        }
+                    }
+                ],
+                responses: {
+                    '200': {
+                        description: "Liked"
+                    }
+                }
+            }
+        },
+        '/deleteblog/{id}': {
+            delete: {
+                summary: "user deleting blog",
+                tags: ["blog"],
+                parameters: [
+                    {
+                        name: "id",
+                        in: "path",
+                        required: true,
+                        schema: {
+                            type: "string"
+                        }
+                    }
+                ],
+                responses: {
+                    '200': {
+                        description: "Deleted successfully"
+                    },
+                    '400': {
+                        description: "Unauthorized"
+                    }
+                }
+            }
+        },
+        '/deletemessage/{id}': {
+            delete: {
+                summary: "user deleting message",
+                tags: ["message"],
+                parameters: [
+                    {
+                        name: "id",
+                        in: "path",
+                        required: true,
+                        schema: {
+                            type: "string"
+                        }
+                    }
+                ],
+                responses: {
+                    '200': {
+                        description: "Deleted successfully"
+                    },
+                    '400': {
+                        description: "Unauthorized"
+                    }
+                }
+            }
+        },
+        '/subscriber': {
+            get: {
+                summary: "user get subscriber",
+                tags: ["user"],
+                responses: {
+                    '200': {
+                        description: "Success"
+                    },
+                    '400': {
+                        description: "Unauthorized"
+                    }
+                }
+            }
+        },
+        '/blogs': {
+            get: {
+                summary: "view all blogs",
+                tags: ["blog"],
+                responses: {
+                    '200': {
+                        description: "Success"
+                    }
+                }
+            }
+        },
+        '/message': {
+            get: {
+                summary: "view all message",
+                tags: ["message"],
+                responses: {
+                    '200': {
+                        description: "Success"
+                    },
+                    '400': {
+                        description: "Unauthorized"
+                    }
+                }
+            }
+        },
     },
     components: {
         securitySchemes: {
@@ -80,6 +316,65 @@ const options = {
                         required: true,
                         enum: ['user', 'admin'],
                         default: 'user'
+                    }
+                }
+            },
+            Message: {
+                type: 'object',
+                properties: {
+                    name: {
+                        type: 'string'
+                    },
+                    email: {
+                        type: 'string'
+                    },
+                    text: {
+                        type: 'string'
+                    },
+                    time: {
+                        type: 'string',
+                        format: 'date-time'
+                    }
+                }
+            },
+            Blog: {
+                type: 'object',
+                properties: {
+                    title: {
+                        type: 'string',
+                        required: true
+                    },
+                    content: {
+                        type: 'string',
+                        required: true
+                    },
+                    time: {
+                        type: 'string',
+                        required: true
+                    },
+                    likes: {
+                        type: 'integer',
+                        required: true,
+                        default: 0
+                    }
+                }
+            },
+            Comment: {
+                type: 'object',
+                properties: {
+                    blogId: {
+                        type: 'string',
+                        format: 'uuid',
+                        required: true
+                    },
+                    comment: {
+                        type: 'string',
+                        required: true
+                    },
+                    time: {
+                        type: 'string',
+                        format: 'date-time',
+                        required: true
                     }
                 }
             }
